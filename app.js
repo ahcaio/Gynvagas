eadc6b0b3a4158ad3da080e22171a0a0ae1d4fae
Daniel Gondim <danielgondimdecastro@gmail.com>
2023-06-17 23:18:45 -0300

    feat: inclui módulo do Postgres; corrige endpoint para html teste da entidade empresa; realiza integração entre rotas das entidades Empresa e Candidato

--- app.js
+++ app.js
@@ -2,17 +2,18 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const Empresa = require('./empresa');
+const Candidato = require('./candidato');
 
-const appCandidatoEmpresa = express();
-appCandidatoEmpresa.use(cors());
-appCandidatoEmpresa.use(bodyParser.json());
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
 
-appCandidatoEmpresa.get('/empresa', async (req, res) => {
+app.get('/empresa', async (req, res) => {
   const empresa = await Empresa.findAll();
   res.json(empresa);
 });
 
-appCandidatoEmpresa.get('/empresa/:id', async (req, res) => {
+app.get('/empresa/:id', async (req, res) => {
   const empresa = await Empresa.findById(req.params.id);
   if (!empresa) {
     res.status(404).send('Empresa não encontrada');
@@ -20,17 +21,16 @@ appCandidatoEmpresa.get('/empresa/:id', async (req, res) => {
   }
 });
 
-appCandidatoEmpresa.post('/empresa', async (req, res) => {
+app.post('/empresa', async (req, res) => {
   const { nome, email } = req.body;
   const empresa = new Empresa(null, nome, email);
   await empresa.save();
   res.json(empresa);
 });
 
-appCandidatoEmpresa.put('/empresa/:id', async (req, res) => {
+app.put('/empresa/:id', async (req, res) => {
   const empresa = await Empresa.findById(req.params.id);
   if (!empresa) {
-    //conferir se esse essas mensagens aparecem na aba network
     res.status(404).send('Empresa não encontrada');
   } else {
     const { nome, email } = req.body;
@@ -41,7 +41,7 @@ appCandidatoEmpresa.put('/empresa/:id', async (req, res) => {
   }
 });
 
-appCandidatoEmpresa.delete('/empresa/:id', async (req, res) => {
+app.delete('/empresa/:id', async (req, res) => {
   const empresa = await Empresa.findById(req.params.id);
   if (!empresa) {
     res.status(404).send('Empresa não encontrada');
@@ -50,20 +50,6 @@ appCandidatoEmpresa.delete('/empresa/:id', async (req, res) => {
     res.status(204).send('Empresa removida com sucesso');
   }
 });
-
-// const PORT = process.env.PORT || 3000;
-// appCandidatoEmpresa.listen(PORT, () => {
-//   console.log(`Servidor rodando na porta ${PORT}`);
-// });
-
-// const express = require('express');
-// const bodyParser = require('body-parser');
-// const cors = require('cors');
-const Candidato = require('./candidato');
-
-const app = express();
-app.use(cors());
-app.use(bodyParser.json());
 
 app.get('/candidatos', async (req, res) => {
   const candidatos = await Candidato.findAll();
@@ -78,8 +64,6 @@ app.get('/candidatos/:id', async (req, res) => {
     res.json(candidato);
   }
 });
-
-// corrigir rota de candidatos com os parâmetros corretos (eliminar os produtos)
 app.post('/candidatos', async (req, res) => {
   const { nome, login, email} = req.body;
   const candidato = new Candidato(null, nome, login, email);
@@ -99,7 +83,7 @@ app.put('/candidatos/:id', async (req, res) => {
     candidato.login = login;
     candidato.email = email;
     await candidato.save();
-    res.json(candidato); //200 fica implícito
+    res.json(candidato);
   }
 });
 
